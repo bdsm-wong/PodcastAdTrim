0fa77ff7828c9cf65190f92d2d7af0bf06b4fc38
bdsm_wong <coyne.32@gmail.com>
2026-03-16 14:35:51 -0500

    New util to determine file request type (file vs name)

--- server/app/Utils/AudioFormat.py
+++ server/app/Utils/AudioFormat.py
@@ -4,3 +4,13 @@ class AudioFormat:
         # Check if the file extension is in the list of allowed audio formats
         allowed_formats = ['.mp3', '.wav', '.m4a', '.ogg']
         return any(filename.lower().endswith(format) for format in allowed_formats)
+
+    @staticmethod
+    def request_type(request,key):
+        # determine key is a file or form
+        if key in request.files:
+            return "file"
+        elif key in request.form:
+            return "name"
+        else:
+            return None
