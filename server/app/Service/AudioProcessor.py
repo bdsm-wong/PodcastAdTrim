087905c0dae4cc05c4b6867a1d8f71a46be64b5b
bdsm_wong <coyne.32@gmail.com>
2026-03-17 20:07:38 -0500

    Handle files or names for both full and frag

--- server/app/Service/AudioProcessor.py
+++ server/app/Service/AudioProcessor.py
@@ -96,7 +96,8 @@ class AudioProcessor:
         return {'error': False}
     
     def _show_elements(self, show_elements_array):
-        elements = {'location_in_seconds': self._find_segment_audio.exact_second}
+        _ = self._find_segment_audio.exact_second
+        elements = {'location_in_seconds': _}
         if 'total_execution_time' in show_elements_array:
             elements['total_execution_time'] = self._total_execution_time
 
