e5a96e00e33b67249cf960d9859111602af65bad
bdsm_wong <coyne.32@gmail.com>
2026-03-29 10:49:47 -0500

    Add LOG_LEVEL .env variable

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
+import logging
 from dotenv import load_dotenv
 import os
-import logging
+
+logging.basicConfig(filename='audio_detection_service.log', format=f'%(asctime)s %(levelname)s : %(message)s')
+
+app = Flask(__name__)
 
 load_dotenv()
 logLevelStr = os.getenv('LOG_LEVEL', default='ERROR').upper()
-logging.basicConfig(filename='audio_detection_service.log', level=logLevelStr, format=f'%(asctime)s %(levelname)s : %(message)s')
 
-app = Flask(__name__)
+try:
+    app.logger.setLevel(logLevelStr)
+except ValueError:
+    app.logger.setLevel(logging.ERROR)
+    app.logger.ERROR(f"Invalid Log Level in .env file: {logLevelStr}.  ERROR used instead.")
 
 from .Service import AudioProcessor
 from .Routes import api
