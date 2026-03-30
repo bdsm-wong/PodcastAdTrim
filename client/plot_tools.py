9c4c4fb4e0bd986dfb10a978c346f469c3283056
bdsm_wong <coyne.32@gmail.com>
2026-03-30 14:33:34 -0500

    Updating plot_tools to show full, frag, and corr matrices

--- client/plot_tools.py
+++ client/plot_tools.py
@@ -2,37 +2,95 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Matrix:
-	def __init__(self,npz_dir,npz_file):
-		self.npz_dir = npz_dir
-		self.npz_file = npz_file
+	def __init__(self,storage_dir, full_name, frag_name):
+		self._storage_dir = storage_dir
+		self._full_name = full_name
+		self._frag_name = frag_name
 
 	def _load_data(self):
-		self.npz_path = self.npz_dir + "/" + self.npz_file + ".npz"
-		_data = np.load(file=self.npz_path)
-		self.full_audio = _data['audio_matrix']
-		num_samples = len(self.full_audio)
-		print("num_samples: ", num_samples)
+		# Build file paths
+		_full_path = self._storage_dir + "/full/" + self._full_name + ".npz"
+		_frag_path = self._storage_dir + "/frag/" + self._frag_name + ".npz"
+		_corr_path = self._storage_dir + "/corr/" + self._full_name + "/" + self._frag_name + ".npz"
 
-		# generate clock data from sample rate (e.g. 44,100 Hz) and duration
-		self.sample_rate = _data['sample_rate']
-		print("sample_rate: ", self.sample_rate, " Hz")
-		self.duration = _data['audio_duration']
-		print("duration: ", self.duration, " seconds")
-		self.clock = np.linspace(start=0,stop=self.duration,num=num_samples)
+		# Load full matrix data
+		_full_data = np.load(file=_full_path)
+		self._full_audio = _full_data['audio_matrix']
+		_full_num_samples = len(self._full_audio)
+		print(f"full audio: {str(_full_num_samples)} samples")
+		_sample_rate = _full_data['sample_rate']
+		print(f"sample_rate: {str(_sample_rate)} Hz")
+
+		# Generate clock signal
+		_full_duration = _full_data['audio_duration']
+		print(f"full_duration: {str(_full_duration)} seconds")
+		self.clock = np.linspace(start=0, stop=_full_duration, num=_full_num_samples)
+
+		# Load and pad correlation data
+		_corr_data = np.load(file=_corr_path)
+		_corr_raw = _corr_data['corr_matrix']
+		_corr_num_samples = len(_corr_raw)
+		_corr_rPad_len = _full_num_samples - _corr_num_samples
+		self._corr_padded = np.pad(
+			array=_corr_raw,
+			pad_width=(0, _corr_rPad_len),
+			mode='constant',
+			constant_values=(0, 0)
+		)
+		self._exact_second = _corr_data['exact_second']
+		print("exact_second: ", self._exact_second)
+
+		# Load and shift fragment matrix data
+		_frag_data = np.load(file=_frag_path)
+		_frag_raw = _frag_data['audio_matrix']
+		_frag_num_samples = len(_frag_raw)
+		_frag_lPad_len = int(self._exact_second * _sample_rate)
+		_frag_rPad_len = _full_num_samples - _frag_num_samples - _frag_lPad_len
+		self._frag_aligned = np.pad(
+			array=_frag_raw,
+			pad_width=(_frag_lPad_len, _frag_rPad_len),
+			mode='constant',
+			constant_values=(0, 0)
+		)
+
 
 	def plot_data(self):
+		# Load data and set up chart
 		self._load_data()
-		fig, (_axis_full) = plt.subplots(nrows=1, ncols=1)
-		_axis_full.plot(self.clock, self.full_audio)
-		_axis_full.set_title('Full Audio')
-		_axis_full.set_xlabel("Elapsed Time (s)")
-		_axis_full.set_ylabel("Signal Amplitude")
-		_axis_full.set_title(self.npz_file)
+
+		fig, (_ax_corr) = plt.subplots(nrows=1, ncols=1)
+		#fig, (_ax_full, _ax_corr, _ax_frag) = plt.subplots(nrows=3, ncols=1)
+
+		'''
+		# Plot full audio matrix
+		_ax_full.plot(self.clock, self._full_audio)
+		_ax_full.set_title(f"Full Audio: {self._full_name}")
+		_ax_full.set_xlabel("Elapsed Time (s)")
+		_ax_full.set_ylabel("Signal Amplitude")
+		'''
+
+		# Plot correlation data
+		_ax_corr.plot(self.clock, self._corr_padded)
+		_ax_corr.set_title("Correlation Matrix")
+		_ax_corr.set_xlabel("Elapsed Time (s)")
+		_ax_corr.set_ylabel("Correlation Strength")
+		_window_width = 100000000
+		#_ax_corr.set(xlim=(self._exact_second - (_window_width/2), self._exact_second + (_window_width/2)))
+
+		'''
+		# Plot fragment matrix
+		_ax_frag.plot(self.clock, self._frag_aligned)
+		_ax_frag.set_title(f"Fragment Audio: {self._frag_name}")
+		_ax_frag.set_xlabel("Elapsed Time (s)")
+		_ax_frag.set_ylabel("Signal Amplitude")
+		'''
+
 		plt.show()
 
 #load matrices from 'storage' directory
-storage_dir = '/media/ubuntu01/AudioTimeStamp-Detective/storage'
+_storage_dir = '/home/jeremy/Desktop/storage'
 #test_file = 'test'
-test_file = 'full/1997-04-02 (Guest - no guest)'
-test_matrix = Matrix(storage_dir,test_file)
-test_matrix.plot_data()
+_full_name = '1997-04-02 (Guest - no guest)'
+_frag_name = 'Podcast_One_Intro'
+debug_matrix = Matrix(storage_dir=_storage_dir, full_name=_full_name, frag_name=_frag_name)
+debug_matrix.plot_data()
